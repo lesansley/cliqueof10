815fa82de446a57cfae6c76906c8c3af8f65bbd7
Les Ansley <les@ansleyfamily.com>
2024-06-14 17:18:27 +0200

    Fixed env reference

--- server/firebase-config.js
+++ server/firebase-config.js
@@ -1,5 +1,5 @@
 export default {
-  type: process.env.FIREBASE_SERVICE_ACCOUNT,
+  type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY,
